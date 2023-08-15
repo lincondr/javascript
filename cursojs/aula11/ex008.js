888066beab08e3c68f6c0abd28d23508af7eebe3
Lincon Rodrigues Glicerio <glickrodrigues2312@gmail.com>
2023-08-15 07:43:58 -0300

    condiçoes

---
+++ cursojs/aula11/ex008.js
@@ -0,0 +1,5 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é de ${vel}km/h`)
+if (vel > 60) { // Condiçao simples
+    console.log('Voce ultrapassou a velocidade permitida. Multado!')
+}
---
+++ cursojs/aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'EUA'
+if (pais == 'BRASIL') {
+    console.log(`Vivendo em ${pais} voce é Brasileiro`)
+} else {
+    console.log(`Vivendo em ${pais} voce é estrangeiro`)
+}
