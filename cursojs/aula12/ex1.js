d8a536f0a4e4055e62a93029f5e2bcf0029aa6a3
Lincon Rodrigues Glicerio <glickrodrigues2312@gmail.com>
2023-08-16 10:44:05 -0300

    aula 12 exercicios

---
+++ cursojs/aula12/ex1.js
@@ -0,0 +1,13 @@
+var pontos = 10
+console.log(`Pontuação maxima: ${pontos}`)
+if (pontos > 99){
+    console.log(`impossible`)
+} else if (pontos >=80){
+    console.log('incredible')
+} else if (pontos >= 60){
+    console.log('amazing')
+} else if (pontos >= 45) {
+    console.log('great')
+} else {
+    console.log('good')
+}
---
+++ cursojs/aula12/ex2.js
@@ -0,0 +1,36 @@
+var gol1 = 1
+console.log(`Corinthians has scored ${gol1} gols.`)
+var gol2 = 1
+console.log(`Sao Paulo has scored ${gol2} gols.`)
+
+if (gol1 > gol2) {
+    console.log('Corinthians wins')
+} else if (gol1 < gol2) {
+    console.log('Sao Paulo wins')
+} else {
+    console.log('It is a tie')
+}
+
+// switch(gols) {
+//     case 0:
+//         console.log('Need improvement')
+//         break
+//     case 1:
+//         console.log('Could more')
+//         break
+//     case 2:
+//         console.log('A good job!')
+//         break
+//     case 3:
+//         console.log('Solid win!')
+//         break
+//     case 4:
+//         console.log('Amazing match!')
+//         break
+//     case 5:
+//         console.log('Flawless victory!')
+//         break
+//     default:
+//         console.log('Result is not right')
+//         break
+// }
