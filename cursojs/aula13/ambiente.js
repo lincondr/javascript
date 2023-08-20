fc68761e5f21275588399cb9ca0575a96c12c00a
Lincon Rodrigues Glicerio <glickrodrigues2312@gmail.com>
2023-08-20 09:36:00 -0300

    aulas 13 e 14

--- cursojs/aula13/ambiente.js
+++ cursojs/aula13/ambiente.js
@@ -1,13 +1,3 @@
-var a = 12
-var b = 23
-a += b
-console.log(`a vale ${a} e b vale ${b}`)
-var s = a + b
-
-while (s <=100) {
-    console.log(`a vale ${a} e b vale ${b}, juntos valem ${s}`)
-    a += a
-}
 
 
 // while (c <= 6) {
---
+++ cursojs/aula14/ambiente.js
@@ -0,0 +1,5 @@
+console.log('Vai começar...')
+for(var c = 1;c <= 21;c += 5) {
+    console.log(c)
+}
+console.log('FIM!')
