7c5f7aa05bc825c391c3bd3b66da77a42e7ea9c9
Lincon Rodrigues Glicerio <glickrodrigues2312@gmail.com>
2023-08-24 09:26:27 -0300

    aula 16

---
+++ cursojs/aula16/funcao01.js
@@ -0,0 +1,10 @@
+function parimp(n) {
+    if (n%2 == 0) {
+        return 'O numero é Par'
+    } else {
+        return 'O numero é Impar'
+    }
+}
+
+let res = parimp(71)
+console.log(res)
---
+++ cursojs/aula16/funcao03.js
@@ -0,0 +1,6 @@
+//É possivel adicionar uma funçao a uma variavel
+let v = function(x) {
+    return x*2
+}
+
+console.log(v(5))
